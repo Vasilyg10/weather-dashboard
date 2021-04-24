172b446bcc85ed98d922283fa7ff4189b9d632c7
Vasilyg10 <zvgianna@gmail.com>
2021-04-23 20:10:45 -0500

    nested fetches to pull correct data

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+let city = 'Athens';
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6ada8502946e81fb6b7659868e983f5b`)
+.then(response => response.json())
+.then(currentData => {
+    let lat = currentData.coord.lat;
+    let lon = currentData.coord.lon;
+    console.log(currentData)
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly&units=imperial&appid=6ada8502946e81fb6b7659868e983f5b`)
+        .then(response => response.json())
+        .then(data => {
+            const currentTemp = data.daily[0].temp.day;
+            const currentWind = data.daily[0].wind_speed;
+            const currentHumidity = data.daily[0].humidity;
+            const currentUvindex = data.daily[0].uvi;
+            for (let i = 1; i < 6; i++) {
+                const nextTemp = data.daily[i].temp.day;
+                const nextWind = data.daily[i].wind_speed;
+                const nextHumidity = data.daily[i].humidity;
+                const nextUvindex = data.daily[i].uvi;
+            }
+            console.log(data.daily[0].temp.day)
+            console.log(data)
+        });
+});
+
